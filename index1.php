36b269f9c87e9423569662a74b1bb6d75f98656f
milaasari <2002milasari@gmail.com>
2022-10-04 14:35:58 +0700

    first commit

---
+++ index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+  <title>DATA DIRI </title>
+</head>
+<body>
+<center><h1 style="color: green; margin-top: 2px">Identitas Diri Saya </h1><hr>
+<font size="5" face="Century Gothic"><p align=>my lil bio</p></font>
+<img src="moo.jpg" style="width: 150px;height: 250px;border-radius: 100%;"></br>
+	<font size="5" face="Century Gothic">  Mila Sari Rusadi</br>
+	  20106050073</br>
+	 Teknik Infromatika</br></br>
+	 UIN SUNAN KALIJAGA </br></font>
+</body></html>
